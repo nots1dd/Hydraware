1082c9099d2de3259d36b8def25508b9760e0809
AltSumpreme <reubenphilip52009@gmail.com>
2024-02-25 20:11:15 +0530

    chore: Revamp ScreenA

--- lib/pages/intro_screens/intro_screenA.dart
+++ lib/pages/intro_screens/intro_screenA.dart
@@ -6,21 +6,21 @@ class ScreenA extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue[600],
+      color: Colors.blue[800],
       child: Column(
         children: [
           const SizedBox(height: 60),
           Text('Welcome to Hydraware',
               style: TextStyle(
-                  color: Colors.blue[850],
+                  color: Colors.black,
                   fontSize: 24,
                   fontFamily: 'Cera Pro',
                   fontWeight: FontWeight.bold)),
           const SizedBox(height: 20),
           Container(
-              color: Colors.blue[600],
+              color: Colors.blue[800],
               child: Image.asset(
-                'assets/images/logo.png',
+                'assets/images/icon_alt.png',
                 width: 200,
                 height: 200,
                 fit: BoxFit.cover,
@@ -28,8 +28,8 @@ class ScreenA extends StatelessWidget {
           const SizedBox(height: 20),
           const Text('Your app for everything hydro!',
               style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+                  color: Colors.black,
+                  fontSize: 22,
                   fontFamily: 'Cera Pro',
                   fontWeight: FontWeight.w200)),
           const SizedBox(height: 20),
